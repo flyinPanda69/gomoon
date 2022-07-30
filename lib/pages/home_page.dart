6ae05ebf8c3d70b7b431375cd6540e937d25957c
Rahul Anand <rahul.anand3@gds.ey.com>
2022-07-30 22:39:23 +0530

    gomoonappfinal

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:go_moon/widgets/custom_dropdown_button.dart';
+
+class HomePage extends StatelessWidget {
+  late double _deviceHeight, _deviceWidth;
+
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+            height: _deviceHeight,
+            width: _deviceWidth,
+            padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+            child: Stack(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [_pageTitle(), _bookRideWidget()],
+                ),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: _astroImageWidget())
+              ],
+            )),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+          color: Colors.white, fontSize: 70, fontWeight: FontWeight.w800),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      height: _deviceHeight * 0.50,
+      width: _deviceWidth * 0.65,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.contain,
+          image: AssetImage("assets/images/astro_moon.png"),
+        ),
+      ),
+    );
+  }
+
+  Widget _bookRideWidget() {
+    return Container(
+      height: _deviceHeight * 0.25,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _destinationDropDownWidget(),
+          _travellersInformationWidget(),
+          _rideButtonWidget()
+        ],
+      ),
+    );
+  }
+
+  Widget _destinationDropDownWidget() {
+    List<String> _items = ['James web station', 'International space station'];
+    return CustomDropDownButtonClass(value: _items, width: _deviceWidth);
+  }
+
+  Widget _travellersInformationWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CustomDropDownButtonClass(
+            value: ['1', '2', '3', '4'], width: _deviceWidth * 0.45),
+        CustomDropDownButtonClass(
+            value: ['Economy', 'Business', 'Private'],
+            width: _deviceWidth * 0.40),
+      ],
+    );
+  }
+
+  Widget _rideButtonWidget() {
+    return Container(
+      width: _deviceWidth,
+      margin: EdgeInsets.only(bottom: _deviceHeight * 0.01),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: MaterialButton(
+          onPressed: () {},
+          child: const Text(
+            'Book Ride!',
+            style: TextStyle(color: Colors.black),
+          )),
+    );
+  }
+}
---
+++ lib/widgets/custom_dropdown_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomDropDownButtonClass extends StatelessWidget {
+  List<String> value;
+  double width;
+
+  CustomDropDownButtonClass({required this.value, required this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: width * 0.05),
+      width: width,
+      decoration: BoxDecoration(
+          color: const Color.fromRGBO(53, 53, 53, 1.0),
+          borderRadius: BorderRadius.circular(10)),
+      child: DropdownButton(
+        value: value.first,
+        onChanged: (_) {},
+        items: value
+            .map((e) => DropdownMenuItem(
+                  child: Text(e),
+                  value: e,
+                ))
+            .toList(),
+        underline: Container(),
+        dropdownColor: const Color.fromRGBO(53, 53, 53, 1.0),
+        style: const TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
